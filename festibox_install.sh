6da08c2a1e23a477ed545049fdfd295804ee70d8
regisburin <regisestuncool@gmail.com>
2016-06-30 09:20:49 +0200

    Update festibox_install.sh

--- festibox_install.sh
+++ festibox_install.sh
@@ -1,8 +1,10 @@
 ## WIP ##
-sudo apt-get install lighttpd dnsmasq hostapd iw
+
+
+PB_DIR=/opt/piratebox
+
 wget http://downloads.piratebox.de/piratebox-ws_current.tar.gz
 tar xzf piratebox-ws_current.tar.gz
 cd piratebox #(le "vrai" dossier PB est dans le dossier dézippé!)
-sudo cp piratebox /opt/
-cd /opt/piratebox
-sudo bash bin/install_piratebox.sh ../conf/piratebox.conf imageboard pyForum # (là, erreur dûe au dossier d'Upload/data = FAT32, à voir avec un ln -s...)
+sudo cp -r piratebox /opt/
+sudo bash $PB_DIR/bin/install_piratebox.sh $PB_DIR/conf/piratebox.conf part2
